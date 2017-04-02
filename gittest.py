57143179376dd6f72373700633eb37051567960b
H Henson <economisthenson@gmail.com>
2017-04-02 09:26:41 -0400

    Initial Seed File to start the test
    Toy program to test out the issues with committing programs

---
+++ gittest.py
@@ -0,0 +1,12 @@
+# This is a python script for use in testing the resolution of differences in version
+# version 1 on Harolds Computer
+
+VAR1 = 10
+
+VAR2 = 2
+
+varlist = [VAR1,VAR2]
+
+print("VAR1 = {} VAR2 = {}".format(varlist))
+
+
